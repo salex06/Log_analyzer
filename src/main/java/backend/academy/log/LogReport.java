bcd6ec49c272f262105ad7d9206a1872f0ee9d86
Alexey Sazonov <salex.9206@mail.ru>
2024-10-23 17:34:53 +0300

    feat: complete the list of codes

--- src/main/java/backend/academy/log/LogReport.java
+++ src/main/java/backend/academy/log/LogReport.java
@@ -1,11 +1,8 @@
 package backend.academy.log;
 
 import java.time.LocalDate;
-import java.util.AbstractMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public record LogReport(
     List<String> files,
@@ -19,15 +16,78 @@ public record LogReport(
 ) {
 
     @SuppressWarnings("checkstyle:magicnumber")
-    public static final Map<Short, String> CODES = Stream.of(
-        new AbstractMap.SimpleImmutableEntry<>((short) 200, "OK"),
-        new AbstractMap.SimpleImmutableEntry<>((short) 301, "Moved permanently"),
-        new AbstractMap.SimpleImmutableEntry<>((short) 302, "Found"),
-        new AbstractMap.SimpleImmutableEntry<>((short) 304, "Not modified"),
-        new AbstractMap.SimpleImmutableEntry<>((short) 401, "Unauthorized"),
-        new AbstractMap.SimpleImmutableEntry<>((short) 403, "Forbidden"),
-        new AbstractMap.SimpleImmutableEntry<>((short) 404, "Not found"),
-        new AbstractMap.SimpleImmutableEntry<>((short) 500, "Internal server error"),
-        new AbstractMap.SimpleImmutableEntry<>((short) 504, "Gateway timeout")
-    ).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    public static final Map<Short, String> CODES = Map.<Short, String>ofEntries(
+        Map.entry((short) 100, "Continue"),
+        Map.entry((short) 101, "Switching protocols"),
+        Map.entry((short) 102, "Processing"),
+        Map.entry((short) 103, "Early Hints"),
+        Map.entry((short) 200, "OK"),
+        Map.entry((short) 201, "Created"),
+        Map.entry((short) 202, "Accepted"),
+        Map.entry((short) 204, "No Content"),
+        Map.entry((short) 205, "Reset Content"),
+        Map.entry((short) 206, "Partial Content"),
+        Map.entry((short) 207, "Multi-Status"),
+        Map.entry((short) 208, "Already Reported"),
+        Map.entry((short) 226, "IM Used"),
+        Map.entry((short) 300, "Multiple Choices"),
+        Map.entry((short) 301, "Moved Permanently"),
+        Map.entry((short) 302, "Found"),
+        Map.entry((short) 303, "See Other"),
+        Map.entry((short) 304, "Not Modified"),
+        Map.entry((short) 305, "Use Proxy"),
+        Map.entry((short) 307, "Temporary Redirect"),
+        Map.entry((short) 308, "Permanent Redirect"),
+        Map.entry((short) 400, "Bad Request"),
+        Map.entry((short) 401, "Unauthorized"),
+        Map.entry((short) 402, "Payment Required"),
+        Map.entry((short) 403, "Forbidden"),
+        Map.entry((short) 404, "Not Found"),
+        Map.entry((short) 405, "Method Not Allowed"),
+        Map.entry((short) 406, "Not Acceptable"),
+        Map.entry((short) 407, "Proxy Authentication"),
+        Map.entry((short) 408, "Request Timeout"),
+        Map.entry((short) 409, "Conflict"),
+        Map.entry((short) 410, "Gone"),
+        Map.entry((short) 411, "Length Required"),
+        Map.entry((short) 412, "Precondition Failed"),
+        Map.entry((short) 413, "Payload Too Large"),
+        Map.entry((short) 414, "URI Too Long"),
+        Map.entry((short) 415, "Unsupported Media Type"),
+        Map.entry((short) 416, "Range Not Satisfiable"),
+        Map.entry((short) 417, "Expectation Failed"),
+        Map.entry((short) 418, "I’m a teapot"),
+        Map.entry((short) 419, "Authentication Timeout"),
+        Map.entry((short) 421, "Misdirected Request"),
+        Map.entry((short) 422, "Unprocessable Entity"),
+        Map.entry((short) 423, "Locked"),
+        Map.entry((short) 424, "Failed Dependency"),
+        Map.entry((short) 425, "Too Early"),
+        Map.entry((short) 426, "Upgrade Required"),
+        Map.entry((short) 428, "Precondition Required"),
+        Map.entry((short) 429, "Too Many Requests"),
+        Map.entry((short) 431, "Request Header Fields Too Large"),
+        Map.entry((short) 449, "Retry With"),
+        Map.entry((short) 451, "Unavailable For Legal Reasons"),
+        Map.entry((short) 499, "Client Closed Request"),
+        Map.entry((short) 500, "Internal Server Error"),
+        Map.entry((short) 501, "Not Implemented"),
+        Map.entry((short) 502, "Bad Gateway"),
+        Map.entry((short) 503, "Service Unavailable"),
+        Map.entry((short) 504, "Gateway Timeout"),
+        Map.entry((short) 505, "HTTP Version Not Supported"),
+        Map.entry((short) 506, "Variant Also Negotiates"),
+        Map.entry((short) 507, "Insufficient Storage"),
+        Map.entry((short) 508, "Loop Detected"),
+        Map.entry((short) 509, "Bandwidth Limit Exceeded"),
+        Map.entry((short) 510, "Not Extended"),
+        Map.entry((short) 511, "Network Authentication Required"),
+        Map.entry((short) 520, "Unknown Error"),
+        Map.entry((short) 521, "Web Server Is Down"),
+        Map.entry((short) 522, "Connection Timed Out"),
+        Map.entry((short) 523, "Origin Is Unreachable"),
+        Map.entry((short) 524, "A Timeout Occurred"),
+        Map.entry((short) 525, "SSL Handshake Failed"),
+        Map.entry((short) 526, "Invalid SSL Certificate")
+    );
 }
